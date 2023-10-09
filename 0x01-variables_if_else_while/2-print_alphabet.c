479409ece9c865fb06d22d1280492e8bb7760bba
amrrefaey <saifuallah19@gmail.com>
2023-10-09 04:24:51 +0200

    ,,

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,13 +5,11 @@
  *
  * Return: Always 0 (Success)
  **/
-int main(void)
-{
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i = 0; i < 26; i++)
-		putchar(alp[i]);
-		putchar('\n');
-	return (0);
+int main(void){
+	char letter = 'a';
+	do{
+		(putchar(letter), letter ++);
+	} while (letter <= 'z');
+	putchar('\n');
+	return 0;
 }
