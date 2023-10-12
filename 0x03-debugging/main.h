47d2e04c4d208d9d3c20f795c16f2209044aa84c
amrrefaey <saifuallah19@gmail.com>
2023-10-12 22:03:41 +0200

    why?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,15 +6,3 @@
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
-
-// #include <stdlib.h>
-//#include <time.h>
-/**\* betty style doc for function main goes there */
-//int main(void)
-//{
-//		int n;
-//
-//			srand(time(0));
-//				n = rand() - RAND_MAX / 2;
-//					return (0);
-//}
