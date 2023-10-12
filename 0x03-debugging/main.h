ef6b6ef35575b04e06106ad062c8e53ff8b2a6d2
amrrefaey <saifuallah19@gmail.com>
2023-10-12 22:00:20 +0200

    hard

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,20 @@
-#include <stdlib.h>
-#include <time.h>
-/* betty style doc for function main goes there */
-int main(void)
-{
-		int n;
+#ifndef MAIN_H
+#define MAIN_H
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					return (0);
-}
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
+
+// #include <stdlib.h>
+//#include <time.h>
+/**\* betty style doc for function main goes there */
+//int main(void)
+//{
+//		int n;
+//
+//			srand(time(0));
+//				n = rand() - RAND_MAX / 2;
+//					return (0);
+//}
