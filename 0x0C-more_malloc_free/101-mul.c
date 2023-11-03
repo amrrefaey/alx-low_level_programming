429925489e15ed2a23888fcd048ea1a5917a84ba
amrrefaey <saifuallah19@gmail.com>
2023-11-03 02:45:10 +0200

    4

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int look(char *str);
+char *make_array(int size);
+char *iterate_0s(char *str);
+void get_prod(char *prod, char *mult, int digit, int zeroes);
+void add_numbers(char *final_prod, char *next_prod, int next_len);
