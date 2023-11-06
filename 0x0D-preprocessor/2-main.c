3227408e44f1e3dd4e6b116a5a70e0cb3411c506
amrrefaey <saifuallah19@gmail.com>
2023-11-06 22:48:22 +0200

    fuck betty

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /*
- * main - Function that prints the name of the file
+ * main - The Entry point function
  *
+ * Description: 'prints the name of the file'
  * Return: 0 Always success
  */
 int main(void)
